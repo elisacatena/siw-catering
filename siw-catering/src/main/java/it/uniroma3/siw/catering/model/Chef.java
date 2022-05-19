a293e7f421f118d60e9b7260fa9cb3741c95dc14
Elisa Catena <elisacatena00@gmail.com>
2022-05-19 22:36:38 +0200

    Update Chef.java

--- siw-catering/src/main/java/it/uniroma3/siw/catering/model/Chef.java
+++ siw-catering/src/main/java/it/uniroma3/siw/catering/model/Chef.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Chef {
@@ -16,8 +17,13 @@ public class Chef {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
 	private String cognome;
+	
+	@NotBlank
 	private String nazionalita;
 	
 	/* cascade per remove perchè se elimino lo chef elimino anche tutti i buffet da lui proposti */
