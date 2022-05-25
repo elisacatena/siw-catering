6c51ac4ef7aee92c0c9562bb41dd7f3c411633c8
Elisa Catena <elisacatena00@gmail.com>
2022-05-25 15:39:15 +0200

    Revert "Spostato il package validation"
    This reverts commit 4aa1c059cc4482f6b876495a7139a11fa1f2eb22.

--- siw-catering/src/main/java/it/uniroma3/siw/catering/controller/validator/PiattoValidator.java
+++ siw-catering/src/main/java/it/uniroma3/siw/catering/controller/validator/PiattoValidator.java
@@ -1,4 +1,4 @@
-package it.uniroma3.siw.catering.validator;
+package it.uniroma3.siw.catering.controller.validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
